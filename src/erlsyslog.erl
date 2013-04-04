8a2d7ce43d58b5b5f74df4c966b317c1a65d965a
Peter Lemenkov <lemenkov@gmail.com>
2013-04-04 15:36:53 +0200

    Kill bogus function
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- src/erlsyslog.erl
+++ src/erlsyslog.erl
@@ -109,6 +109,3 @@ syslog({Fd, Host, Port}, Who, Facility, Level, Message) when is_pid(Who) ->
 	M = list_to_binary(Message),
 	P = list_to_binary(integer_to_list(Facility bor Level)),
 	gen_udp:send(Fd, Host, Port, <<"<", P/binary, "> ", W/binary, ": ", M/binary, "\n">>).
-
-test() ->
-	io:format("Done!~n").
